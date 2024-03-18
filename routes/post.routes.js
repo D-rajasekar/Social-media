da545ea5932fbaee82fd48b3b6e6c288b015cde6
D-rajasekar <rajasekard2003@gmail.com>
2024-03-18 17:20:21 +0530

    adding feature to main

--- routes/post.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import postController from "../controllers/post.controller.js";
-
-const router = express.Router();
-
-router.route("/").post(postController.createPost);
-router.route("/:id").put(postController.updatePost);
-router.route("/comments").post(postController.createcomment);
-export default router;
